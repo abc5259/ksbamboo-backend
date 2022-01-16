7483ec0a8f6ffcf6ff31f849e3dc34496bfe7ed8
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-16 23:47:24 +0900

    feat: create createBaordDto and using createBoardDto in controller and service

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = []; //private를 사용하지 않으면 다른 컨포넌트에서 boards를 수정할 수 있기때문이다.
+  private boards: Board[] = [];
 
   getAllBoards(): Board[] {
     return this.boards;
   }
 
-  createBoard(title: string, description: string): Board {
+  createBoard({ title, description }: CreateBoardDto): Board {
     const board: Board = {
       id: uuid(),
       title,
