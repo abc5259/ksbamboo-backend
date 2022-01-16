1bb2283c6d8f4f468c4bfe9bfdba715b2f0e15a5
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-16 23:31:31 +0900

    feat: create Board clear

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = []; //private를 사용하지 않으면 다른 컨포넌트에서 boards를 수정할 수 있기때문이다.
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string): Board {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
   }
 }
