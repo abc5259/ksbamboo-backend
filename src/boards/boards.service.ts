fe27209b0b068d250cd4e419f5e534d81f704036
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-16 23:10:52 +0900

    feat: create fake Board Model

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = []; //private를 사용하지 않으면 다른 컨포넌트에서 boards를 수정할 수 있기때문이다.
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+}
