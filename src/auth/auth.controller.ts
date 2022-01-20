6e1903489fe4b1393d44b8c9da067b835cd7b1aa
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-21 02:12:20 +0900

    feat: clear user login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,4 +13,11 @@ export class AuthController {
   ): Promise<User> {
     return this.authService.signUp(authCredentialDto);
   }
+
+  @Post('/login')
+  login(
+    @Body(ValidationPipe) authCredentialDto: AuthCredentialDto,
+  ): Promise<string> {
+    return this.authService.login(authCredentialDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -13,5 +14,17 @@ export class AuthService {
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<User> {
     return this.userRepository.createUser(authCredentialDto);
+  }
+
+  async login({ username, password }: AuthCredentialDto): Promise<string> {
+    const user = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException('존재하지 않는 사용자입니다.');
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      throw new UnauthorizedException('비밀번호가 틀립니다.');
+    }
+    return Object.assign({ ok: true });
   }
 }
