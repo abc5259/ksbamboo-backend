49966f33b6b86c7be293fdee7824943009134d18
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-22 00:25:48 +0900

    feat: Using Logger

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   ParseIntPipe,
   Patch,
@@ -23,6 +24,7 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 @Controller('boards')
 @UseGuards(AuthGuard())
 export class BoardsController {
+  private logger = new Logger('BoardController');
   constructor(private boardsService: BoardsService) {}
 
   @Get()
@@ -32,6 +34,7 @@ export class BoardsController {
 
   @Get('/me')
   getMeBoards(@GetUser() user: User) {
+    this.logger.verbose(`User: ${user.username} trying to get all Boards`);
     return this.boardsService.getMeBoards(user);
   }
 
@@ -46,6 +49,11 @@ export class BoardsController {
     @Body() createBoardDto: CreateBoardDto,
     @GetUser() user: User,
   ): Promise<Board> {
+    this.logger.verbose(
+      `User: ${user.username} creating a new board. payload: ${JSON.stringify(
+        createBoardDto,
+      )}`,
+    );
     return this.boardsService.createBoard(createBoardDto, user);
   }
 
